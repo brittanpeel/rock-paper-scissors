f1bc9ea1e0af5371247fd0eeb53a40f1be2ddd5e
Brittan Peel <92232598+brittanpeel@users.noreply.github.com>
2022-09-25 11:22:46 -0600

    added things

--- rps.js
+++ rps.js
@@ -1,42 +1,37 @@
-const array = ['Rock', 'Paper', 'Scissors'];
+const array = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    const computerSelection = array[Math.floor(Math.random() * array.length)]; 
-    return computerSelection;
+    const computerSelection = array[Math.floor(Math.random() * array.length)];
+    return computerSelection.toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection) {
-    
-    if(playerSelection == "rock" && computerSelection == 'Paper') {
+    if (playerSelection == computerSelection) {
+        return "You Tied";
+    }
+    if (playerSelection == "rock" && computerSelection == "Scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if ((computerSelection = "Paper")) {
         return "You Lose! Paper beats Rock";
     }
-    if(playerSelection == "rock" && computerSelection == 'Rock') {
-        return "You Tied!";
-    }
-    if(playerSelection == "rock" && computerSelection == 'Scissors') {
-        return "You Win! Rock beats Scissors";
-    }
-    if(playerSelection == "paper" && computerSelection == 'Paper') {
-        return "You Tied!";
-    }
-    if(playerSelection == "paper" && computerSelection == 'Rock') {
+    if (playerSelection == "paper" && computerSelection == "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (computerSelection == "Rock") {
         return "You Win! Paper beats Rock";
     }
-    if(playerSelection == "paper" && computerSelection == 'Scissors') {
-        return "You Lose! Scissors beats Paper";
-    }
-    if(playerSelection = "scissors" && computerSelection == 'Paper') {
+    if (playerSelection == "scissors" && computerSelection == "Paper") {
         return "You Win! Scissors beats Paper";
-    }
-    if(playerSelection = "scissors" && computerSelection == 'Rock') {
+    } else if (computerSelection == "Rock") {
         return "You Lose! Rock beats Scissors";
     }
-    if(playerSelection = "scissors" && computerSelection == 'Scissors') {
-        return "You Tied!";
-    }
+}
 
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+function game(playRound) {
+    for (const i = 0; i < 5; i++) {
+        playRound();
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
